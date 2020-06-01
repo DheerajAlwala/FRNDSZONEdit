64a5df56e6b67be7ee2aec39f4097dd73fb88010
DheerajAlwala <dheeraj.alwala09@gmail.com>
2020-06-01 20:37:59 +0530

    database added

--- app/src/main/java/com/example/frndszone/MainActivity.java
+++ app/src/main/java/com/example/frndszone/MainActivity.java
@@ -10,7 +10,7 @@ import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
 public class MainActivity extends AppCompatActivity {
-    private static int SPLASH_TIME=3000;
+    private static int SPLASH_TIME=3500;
     FirebaseUser firebaseUser;
 
     @Override
